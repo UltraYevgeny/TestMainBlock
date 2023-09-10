e1acc1239f5c0e5fb57f1ddb5c22a86313a7f5a6
Yevgeny <mr.paranoi@mail.ru>
2023-09-10 10:01:57 +0700

    write main program

--- Program.cs
+++ Program.cs
@@ -10,3 +10,18 @@
     }
     return newArrayCount;
 }
+
+string[] array = {"Lazzer", "13", "Sub Zero", "Q", "2023", "XYZ"};
+string[] newArray = new string[ArrayCount(array)];
+int newArrayCount = 0;
+int letterCount = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    letterCount = array[i].Length;
+    if (letterCount <= 3)
+    {
+        newArray[newArrayCount] = array[i];
+        newArrayCount++;
+    }
+}
